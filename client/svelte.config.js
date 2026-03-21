c37dfa233a902b16e4a7dbf348919adb347f57d5
Aditya81018 <aditya.jaiswal.81018@gmail.com>
2026-03-21 19:59:29 +0530

    vercel build fix

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -1,6 +1,12 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-vercel';
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		adapter: adapter({
+			runtime: 'nodejs24.x'
+		})
+	}
+};
 
 export default config;
